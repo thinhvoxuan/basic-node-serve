d4475a929a80d8b1eac45628d5f2448fdb7deeae
thinhvoxuan <thinhvoxuan@gmail.com>
2016-06-14 18:49:24 +0700

    init project

---
+++ test/index.test.js
@@ -0,0 +1,23 @@
+import { expect }  from 'chai' ;
+import starWar  from '../src/index';
+
+describe('starwars-names', function(){
+
+    it('show work!', function(){
+        expect(true).to.be.true;
+    })
+
+    it('show be array', function(){
+        expect(starWar.all).to.satisfy(isArrayOfString);
+
+        function isArrayOfString(array){
+            return array.every(function(item){
+                return typeof item === 'string';
+            });
+        }
+    })
+    it('should contain thinh voxuan', function(){
+        expect(starWar.all).to.include('dai tran');
+    })
+
+});
