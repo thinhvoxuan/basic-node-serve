25454412938fb81c4140884ed8abe7ee02e71957
thinhvoxuan <thinhvoxuan@gmail.com>
2016-06-14 19:25:12 +0700

    commit package with test and standard file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import fs from 'fs';
-
 export default {
-    all : [
-        'thinh vo',
-        'dai tran'
-    ],
-    random : 'dai tran'
+  all: [
+    'thinh vo',
+    'dai tran'
+  ],
+  random: 'dai tran'
 }
