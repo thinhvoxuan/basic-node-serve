fd95999c759acd4a7315d777ac731c3979ff0079
thinhvoxuan <thinhvoxuan@gmail.com>
2016-06-14 19:36:07 +0700

    add simple router remove build script from project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-export default {
-  all: [
-    'thinh vo',
-    'dai tran'
-  ],
-  random: 'dai tran'
-}
+let express = require('express');
+let app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
