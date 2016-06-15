8e28175267a71a4557c37575695ba30adb4a802f
thinhvoxuan <thinhvoxuan@gmail.com>
2016-06-15 14:52:42 +0700

    make sure return json data

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,11 @@
 let expect = require('chai').expect
 var request = require('superagent');
+
 let boot = require('../bin/index').boot
 let shutdown = require('../bin/index').shutdown
 let port = require('../bin/index').port
 const ROOT_PATH = 'http://localhost:' + port
+
 describe('basic-node-serve', function () {
   before(function () {
     boot()
@@ -14,6 +16,7 @@ describe('basic-node-serve', function () {
       .get(ROOT_PATH)
       .end(function(err, res){
         expect(res.status).to.equal(200);
+        expect(res.header['content-type']).to.have.string('application/json');
         done()
       })
   })
